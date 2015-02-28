027887bc208150320929ae58ebf9630addfdba54
System Administrator <root@Mieszkos-MacBook-Pro.local>
2015-03-01 00:48:05 +0400

    Cont.

--- selext.py
+++ selext.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 #def check_screen_on_all_browsers():
+#driver as global?
 
 def click():
     ''' uses autopy to click element'''
@@ -25,26 +26,56 @@ def check_links(page):
     '''checks all links on a page'''
 
 
-def picture_is_on_page(picture):
-    ''' returns boolean: true if found'''
-    if not picture:
-        return False
-    else:
-        if is_a_picture(picture):
-            #heere we start matching pics
+###### Helpers section, later to be moved to separate file
+def is_a_picture(picture):
+    """Provides simple check if 
+    argument is a picture """
+    pass
 
-        else:
-            return False
+def turn_to_bitmap():
+
+###### End of helpers sections
+
+
+
+
+def picture_is_on_page(driver, picture):
+    ''' returns boolean: True if found'''
+    #if is_a_picture(picture):
+            #heere we start matching pics
+    #else:
+    #    raise TypeError("Image expected, got rotten cabbage instead!")
+    # better approach then the above: piggyback on bitmap functionalities
+    # turn_to_bitmap will check if arg is a picture and throw error if not
+    if not is_instance(driver, selenium.webdriver.webdriver):
+        raise TypeError("Webdriver instance was expected, got rotten cabbage instead!")
+    try:
+        picture = turn_to_bitmap(picture):
+        screen = take_screenshot(driver)
+
+        if picture 
+
+    except:
+        raise TypeError("Image expected, got rotten cabbage instead!")
 
 def text_is_on_page(element):
     ''' returns boolean: true if found'''
-    if not element:
-        return False
-    elif element:
-        #here we start to search
-        #the best way would be to press ctrl-a and copy to clipboard
-    else
-        return False
+    if is_instance(element, [str, unicode]):
+        if len(str)<1:
+            raise TypeError("Got empty string!")
+    else:
+        try:
+            element = str(element)
+        except:
+            raise TypeError("String or stringable thing expected, got rotten cabbage instead!")
+    if element in read_screen:
+        return True 
+    #or maybe better:
+    x = get_coordinates(element)
+    if x['y']>0 and x['x']>0: #(only when coordinates are number higher than 0 text is visible on page)
+        return True 
+    return False
+
 
 def find_on_screen(element):
     '''Finds element by visible text '''
@@ -53,10 +84,28 @@ def find_on_screen(element):
     else
         return None
 
-def find_clickable(browser, element):
+def get_coordinates(driver, element):
+    """ Finds and returns coordinates
+    of a html element
+    by the way:
+    if 
+    """
+    elem = driver.find_element_by_god_knows_what('')
+    coordinates = elem.location
+    # 
+
+def find_and_click():
+    pass
+
+def find_and_type_into():
+    pass
+
+def find_clickable(driver, element):
     ''' Find clickable thing: like a button
     or a link - None if not found
-    args :: browser- instance of a webdriver class'''
+    args :: driver - instance of a webdriver class'''
+    if not is_instance(driver, webdriver):
+        raise TypeError("")
     clickable = browser.find_elements_by_tag_name("a")
     for t in browser.find_elements_by_tag_name("button"):
         clickable.append(t)
@@ -71,11 +120,30 @@ def find_clickable(browser, element):
 def count_elements():
     ''' returns integer - no of elements '''
 
-def read_screen():
+def read_screen(driver):
     '''Returns all text elements from the screen
     visible to the user
     as a list of strings
     '''
+    screentext = 
+    return screentext
+
+def layout_check_with_canny(file_name):
+    """
+    Code used for cross browser checks
+    of layout bugs with canny edge detections
+    """
+    # step 1: open the webpage with all browsers
+    # and take screenshots
+
+    # step 2: treat screenshots with a healthy dose of Canny edge detection
+
+    # step 3: compare all files by calculating distance by pixelwise comparison 
+
+def fuzzy_match_with_canny(file_name):
+    """
+    Code used for cross browser checks
+    """
 
 def fuzzy_match(file_name):
     """Fuzzy image match - takes filename file
